78d3dabb3709d6c921fc5ee5cae0451538a5af8a
siutin <osiutino@gmail.com>
2024-07-22 23:25:05 +0800

    calc rs

--- assets/sw.js
+++ assets/sw.js
@@ -2,8 +2,8 @@ var cacheName = 'egui-template-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  './calc_rs.js',
+  './calc_rs_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
